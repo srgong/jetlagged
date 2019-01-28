015df83cd1b5b5e2beb31167e36be7dd6d3dc533
srgong <g.sharonr@gmail.com>
2019-01-28 11:50:47 -0500

    fix hdfs path

--- src/main/scala/FareStreamer.scala
+++ src/main/scala/FareStreamer.scala
@@ -3,6 +3,7 @@ import java.util.Properties
 import org.apache.kafka.common.serialization.StringSerializer
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
 
 /**
@@ -11,6 +12,9 @@ import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
 object FareStreamer {
   val sparkConf = new SparkConf()
     .setAppName("Fare Streamer")
+//    .setMaster("spark://ec2-18-211-110-36.compute-1.amazonaws.com:7077")
+//    .set("deploy-mode", "cluster")
+//    .set("spark.kafka.broker","spark://ec2-18-211-110-36.compute-1.amazonaws.com:9092")
   val spark: SparkSession =
     SparkSession.builder().config(sparkConf).getOrCreate()
     val sqlContext: SQLContext = spark.sqlContext
@@ -36,7 +40,7 @@ object FareStreamer {
     val STDDEV=60
 
     val withFare = df.withColumn("RIGHTSKEWDISTR", round(randn(seed=10)*STDDEV+MEAN,2))
-    withFare.withColumn("FARE",
+    withFare.withColumn("fare",
       when(col("RIGHTSKEWDISTR") < MIN_FARE, scala.util.Random.nextInt((75 - 35) + 1) + 35)
         .when(col("RIGHTSKEWDISTR") > MAX_FARE, MAX_FARE)
         .otherwise(col("RIGHTSKEWDISTR")))
@@ -48,9 +52,9 @@ object FareStreamer {
     * @return
     */
   def getEpoch(df: DataFrame): DataFrame = {
-    val withTime = df.withColumn("TIME", concat(col("FL_DATE"),lit(" "),col("CRS_DEP_TIME")))
-    val withEpoch = withTime.withColumn("EPOCH",unix_timestamp(col("time"),"yyyy-MM-dd HHmm"))
-    withEpoch.withColumn("TIMESTAMP",to_timestamp(col("EPOCH")))
+    val withTime = df.withColumn("time", concat(col("fl_date"),lit(" "),col("crs_dep_time")))
+    val withEpoch = withTime.withColumn("epoch",unix_timestamp(col("time"),"yyyy-MM-dd HHmm"))
+    withEpoch.withColumn("timestamp",to_timestamp(col("epoch")))
   }
 
   def main(args: Array[String]): Unit = {
@@ -67,9 +71,35 @@ object FareStreamer {
       //    properties.put("buffer.memory", 33554432)
     }
 
+    val customSchema = StructType(Array(
+      StructField("year", StringType, true),
+      StructField("quarter", IntegerType, true),
+      StructField("month", IntegerType, true),
+      StructField("day_of_month", IntegerType, true),
+      StructField("day_of_week", IntegerType, true),
+      StructField("fl_date", StringType, true),
+      StructField("op_unique_carrier", StringType, true),
+      StructField("tail_num", StringType, true),
+      StructField("op_carrier_fl_num", StringType, true),
+      StructField("origin", StringType, true),
+      StructField("origin_city_name", StringType, true),
+      StructField("dest", StringType, true),
+      StructField("dest_city_name", StringType, true),
+      StructField("crs_dep_time", StringType, true),
+      StructField("crs_arr_time", StringType, true),
+      StructField("crs_elapsed_time", DoubleType, true),
+      StructField("distance", DoubleType, true),
+      StructField("distance_group", IntegerType, true)
+    ))
+
 //    val df = sqlContext.read.json("src/main/resources/json").rdd
-    val df = sqlContext.read.option("header", "true").csv("src/main/resources/csv")
-    val withEpoch = getEpoch(df).filter("EPOCH is not null").select("EPOCH","TIMESTAMP","ORIGIN","DEST")
+//    val df = sqlContext.read.option("header", "true").csv("src/main/resources/csv")
+    val df = sqlContext.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+    .schema(customSchema)
+  .csv("hdfs://ec2-18-211-110-36.compute-1.amazonaws.com:9000/user/ubuntu/201712.csv")
+    val withEpoch = getEpoch(df).filter("epoch is not null").select("epoch","timestamp","origin","dest")
     val withReplication = replicate(withEpoch,100)
     val withFare = generateFare(withReplication).rdd
 
