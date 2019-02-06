f3b6e67b038e9f8c5f02c633d4f5b312e5e226de
srgong <g.sharonr@gmail.com>
2019-02-06 13:27:56 -0500

    fix loading page

--- app/jetlagged/views.py
+++ app/jetlagged/views.py
@@ -19,15 +19,15 @@ def before_request():
 @app.route('/index')
 def flights_input():
     # LAX@SLC@2017-12-01T06:30:00.000-05:00
-    keys = g.db.keys("*")
+    # keys = g.db.keys("*")
     flights = []
     # a = [ 'abc=lalalla', 'appa=kdkdkdkd', 'kkakaka=oeoeoeo']
     # flights = dict(s.split('@') for s in keys)
     # print d
     # for flight in flights:
         # print flight
-    for key in keys:
-        print key
+    # for key in keys:
+        # print key
         # flights.append(key.split("@"))
         # dict(key.split('@')
     # return render_template("output.html", flights = flights)
