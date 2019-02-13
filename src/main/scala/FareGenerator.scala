a1a39fe6bda46b3df3f4eba3330a569833913914
srgong <g.sharonr@gmail.com>
2019-02-13 11:05:30 -0500

    write to hdfs

--- src/main/scala/FareGenerator.scala
+++ src/main/scala/FareGenerator.scala
@@ -12,7 +12,7 @@ object FareGenerator {
     .setAppName("Fare Generator")
 //    .setMaster("local[*]")
 //    .set("spark.file.in", "src/main/resources/csv/201803.csv")
-//    .set("spark.file.flights", "src/main/resources/json/")
+//    .set("spark.file.flights", "hdfs://ec2-18-211-110-36.compute-1.amazonaws.com:9000/")
 
   /**
     * Converts csv to json.
@@ -22,7 +22,8 @@ object FareGenerator {
   def save(df: DataFrame, path: String) = {
 //    Saves to hdfs as text file.
 //    df.rdd.saveAsTextFile(path)
-    df.repartition(255).write.mode(SaveMode.Overwrite).format("json").save(path)
+//    df.repartition(255).write.mode(SaveMode.Overwrite).format("json").save(path)
+    df.rdd.saveAsTextFile(path)
   }
 
   /**
